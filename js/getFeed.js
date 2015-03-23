0bf1f149cc683c0acbb7d413479a21b31ea82694
tokka2 <tokka@ipon.biz>
2015-03-23 11:27:05 +0900

    up

--- js/getFeed.js
+++ js/getFeed.js
@@ -1,7 +1,7 @@
     google.load("feeds", "1");
 
     function initialize() {
-      var feed = new google.feeds.Feed("http://fastpshb.appspot.com/feed/1/fastpshb");
+      var feed = new google.feeds.Feed("https://tokka2.github.ip/feed.xml");
       feed.load(function(result) {
         if (!result.error) {
           var container = document.getElementById("feed");
