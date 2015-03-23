a9dce6db25e6b2c409648b005582998b14a8b525
tokka2 <tokka@ipon.biz>
2015-03-23 11:22:35 +0900

    up

--- js/getFeed.js
+++ js/getFeed.js
@@ -1,50 +1,19 @@
-//APIのモジュールを読み込み
-google.load("feeds", "1");
-
-function initialize() {
-	//フィードの取得
-	var feed = new google.feeds.Feed("//tokka2.github.io/feed.xml");
-
-	//エントリの表示数の設定
-	feed.setNumEntries(3);
-
-	//関数の定義
-	feed.load(function(result) {
-		if (!result.error) {
-			//読み込みが成功したときの処理
-			var container = document.getElementById("recent-feed");
-			var htmlstr = "";
-			for (var i = 0; i < result.feed.entries.length; i++) {
-
-				var entry = result.feed.entries[i];
-
-				//日付の取得
-				var pdate = new Date(entry.publishedDate); //Dateクラス
-				var pyear = pdate.getFullYear();   //年
-				var pmonth = pdate.getMonth() + 1; //月
-				var pday = pdate.getDate();        //日
-
-				//日付を2桁表示に変更
-				if (pyear < 2000) pyear += 1900;
-				if (pmonth < 10) {pmonth = "0" + pmonth;}
-				if (pday < 10) {pday = "0" + pday;}
-
-				var date = pyear + "." + pmonth + "." + pday + " ";
-
-				//画像の取得
-				imgsrc = entry.content.match(/src="(.*?)"/igm);
-
-				//html生成
-				htmlstr += '<li><a href="' + entry.link + '">';
-				htmlstr += '<div class="thum"><img ' + imgsrc + ' width="95" height="75"></div>';
-				htmlstr += '<p class="info">' + date + '| ' + entry.categories[0] + '</p>';
-				htmlstr += '<p class="tit">' + entry.title + '</p></a></li>';
+google.load('feeds', '1');
+function initialize(url = '', limit = 6, target = 0) { //引数は適当に切り替える
+	var target = (target == 0) ? '_blank' : '_self';
+	var thumbnail = new Array();
+	var feed = new google.feeds.Feed(url);
+	feed.load(function(rss) {
+		if (!rss.error) {
+			var entry = rss.feed.entries;
+			console.log(rss);
+			for(i = 0; i < limit; i++){
+				thumbnail[i] = entry[i].content.match(/http[s]?\:\/\/[\w\+\$\;\?\.\%\,\!\#\~\*\/\:\@\&\\\=\_\-]+jpg|jpeg|gif|png|bmp/i);
+				$('#rssBox').append('<div class="img_box2"><a href="' + entry[i].link + '" target="' + target + '"><img src="' + thumbnail[i][0] + '" height="' + height + '" width="' + width + '"></a><p class="img_comment3 opa"><a href="' + entry[i].link + '" target="' + target + '">'+ entry[i].title +'</span></a></div>');
 			}
-			container.innerHTML = htmlstr;
-		} else {
-			//読み込みが失敗したときの処理
-			alert(result.error.code + ":" + result.error.message);
 		}
 	});
 }
-google.setOnLoadCallback(initialize);
+google.setOnLoadCallback(function(){
+	initialize('http://tokka2.github.io/feed'); //ここにURL
+});
