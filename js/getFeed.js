d1f397f5f5361a0b395d97c377c626b7af0ce8d1
tokka2 <tokka@ipon.biz>
2015-03-23 11:32:38 +0900

    up

--- js/getFeed.js
+++ js/getFeed.js
@@ -1,17 +1,50 @@
-    google.load("feeds", "1");
+//APIのモジュールを読み込み
+google.load("feeds", "1");
 
-    function initialize() {
-      var feed = new google.feeds.Feed("https://tokka2.github.io/feed.xml");
-      feed.load(function(result) {
+function initialize() {
+    //フィードの取得
+    var feed = new google.feeds.Feed("https://tokka2.github.io/feed.xml");
+
+    //エントリの表示数の設定
+    feed.setNumEntries(30);
+
+    //関数の定義
+    feed.load(function(result) {
         if (!result.error) {
-          var container = document.getElementById("feed");
-          for (var i = 0; i < result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            var div = document.createElement("div");
-            div.appendChild(document.createTextNode(entry.title));
-            container.appendChild(div);
-          }
+            //読み込みが成功したときの処理
+            var container = document.getElementById("feed");
+            var htmlstr = "";
+            for (var i = 0; i < result.feed.entries.length; i++) {
+
+                var entry = result.feed.entries[i];
+
+                //日付の取得
+                var pdate = new Date(entry.publishedDate); //Dateクラス
+                var pyear = pdate.getFullYear();   //年
+                var pmonth = pdate.getMonth() + 1; //月
+                var pday = pdate.getDate();        //日
+
+                //日付を2桁表示に変更
+                if (pyear < 2000) pyear += 1900;
+                if (pmonth < 10) {pmonth = "0" + pmonth;}
+                if (pday < 10) {pday = "0" + pday;}
+
+                var date = pyear + "." + pmonth + "." + pday + " ";
+
+                //画像の取得
+                imgsrc = entry.content.match(/src="(.*?)"/igm);
+
+                //html生成
+                htmlstr += '<li><a href="' + entry.link + '">';
+                htmlstr += '<div class="thum"><img ' + imgsrc + ' width="95" height="75"></div>';
+                htmlstr += '<p class="info">' + date + '| ' + entry.categories[0] + '</p>';
+                htmlstr += '<p class="tit">' + entry.title + '</p></a></li>';
+            }
+            container.innerHTML = htmlstr;
+        } else {
+            //読み込みが失敗したときの処理
+            alert(result.error.code + ":" + result.error.message);
         }
-      });
-    }
-    google.setOnLoadCallback(initialize);
+    });
+}
+google.setOnLoadCallback(initialize);
