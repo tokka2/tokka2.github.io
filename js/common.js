37caa169591e93fcdad2d19a06485708e5f38490
tokka2 <tokka@ipon.biz>
2016-12-03 23:12:57 +0900

    up

--- js/common.js
+++ js/common.js
@@ -11,11 +11,21 @@ TOKKA.TOP.FIXED = {
 		this.$window = $(window);
 		this.$contents = $('.jsc-fixed-contents');
 		this.$target = $('.jsc-fixed-widget');
+		this.targetTop = this.$target.offset().top;
+		this.contentHtml = this.$contents.innerHTML;
 	},
 	bindEvent: function(){
+		var scrollValue = 0;
+		var _self = this;
 		this.$window.on('scroll', function(){
-			console.log(this.$window.scrollTop());
-			console.log(this.$target.offset().top);
+			scrollValue = this.$window.scrollTop();
+			if (scrollValue > _self.targetTop) {
+				console.log(scrollValue);
+				console.log(_self.targetTop);
+				this.$target.addClass('isFixed');
+			} else {
+				this.$target.removeClass('isFixed');
+			}
 		});
 	}
 };
