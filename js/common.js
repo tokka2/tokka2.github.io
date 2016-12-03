169dc3831ffb171a49cb224602bfd23442c6b9a3
tokka2 <tokka@ipon.biz>
2016-12-03 22:18:39 +0900

    up

---
+++ js/common.js
@@ -0,0 +1,17 @@
+var TOKKA = TOKKA || {};
+TOKKA.TOP = {};
+
+TOKKA.TOP.FIXED = {
+
+	init: function(){
+		this.setParameters();
+		this.bindEvent();
+	},
+	setParameters: function(){
+
+	},
+	bindEvent: function(){
+	}
+};
+
+TOKKA.TOP.FIXED.init();
