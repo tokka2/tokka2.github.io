6fce2ee22c6a75251b0836aeaf7dfa31a587b4a9
tokka2 <tokka@ipon.biz>
2016-12-03 23:03:46 +0900

    up

--- js/common.js
+++ js/common.js
@@ -3,19 +3,18 @@ TOKKA.TOP = {};
 
 TOKKA.TOP.FIXED = {
 
-	$window = $(window);
-
 	init: function(){
 		this.setParameters();
 		this.bindEvent();
 	},
 	setParameters: function(){
+		this.$window = $(window);
 		this.$contents = $('.jsc-fixed-contents');
 		this.$target = $('.jsc-fixed-widget');
 	},
 	bindEvent: function(){
-		$window.on('scroll', function(){
-			console.log($window.scrollTop());
+		this.$window.on('scroll', function(){
+			console.log(this.$window.scrollTop());
 			console.log(this.$target.offset().top);
 		});
 	}
